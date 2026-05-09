90710ae48654d6661bba9e04f2d69d7919bf4e9c
Karim Omar <154589762+karim0mar@users.noreply.github.com>
2026-05-09 18:36:21 +0300

    Update API URL for registration endpoint

--- PetClinic/Frontend/src/Components/Register/Register.jsx
+++ PetClinic/Frontend/src/Components/Register/Register.jsx
@@ -18,7 +18,7 @@ export default function Register () {
       setloadingSpinner(true)
       setAPIERR(null)
       let { data } = await axios.post(
-        `${import.meta.env.VITE_API_URL}/auth/register`,
+        `http://localhost:9090/api/auth/register`,
         values,
         { withCredentials: true }
       )
